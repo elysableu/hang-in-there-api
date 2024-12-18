7a4709f009a42f9e8e1a882dd814c4db917d142e
elysableu <elysabward@gmail.com>
2024-12-18 15:23:08 -0700

    Add poster_params helper method to controller

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::PostersController < ApplicationController
     def create
       render json: Poster.create(poster_params)
     end
+    
+    private
+
+    def poster_params
+      params.require(:poster).permit(:name, :description, :price, :year, :vintage, :img_url)
+    end
 end
