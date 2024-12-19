af516c976b0affc8ccf164f5840a23ad61695ffa
Koiree <sequoyah.m.dozier@gmail.com>
2024-12-19 02:11:13 +0100

    Ignore log files in .gitignore

--- spec/models/poster_spec.rb
+++ spec/models/poster_spec.rb
@@ -1,5 +1,24 @@
+#Koiree: This RSpec test checks model validations for the Poster model.
+
 require 'rails_helper'
 
 RSpec.describe Poster, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Describe the context of validations for the Poster model
+  describe 'validations' do
+
+    # Check that a Poster must have a name
+    it { should validate_presence_of(:name) }
+
+    # Check that a Poster must have a description
+    it { should validate_presence_of(:description) }
+
+    # Ensure the price field is present
+    it { should validate_presence_of(:price) }
+
+    # Validate that the price must be a number and greater than zero
+    it { should validate_numericality_of(:price).is_greater_than(0) }
+
+    # Ensure the year field is present
+    it { should validate_presence_of(:year) }
+  end
 end
