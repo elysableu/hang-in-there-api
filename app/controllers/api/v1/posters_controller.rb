debec298433c0af09cdcfe1ef816e74dbfa94401
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-12-19 06:05:12 +0100

    Add conditional handling for destroy endpoint with error response

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::PostersController < ApplicationController
       render json: { error: "Poster not found" }, status: :not_found
     end
   end
-
+  
   def create
     render json: Poster.create(poster_params)
   end
@@ -23,6 +23,28 @@ class Api::V1::PostersController < ApplicationController
   def update
     render json: Poster.update(params[:id], poster_params)
   end
+
+  # DELETE /api/v1/posters/:id
+  def destroy
+    # Directly check for the poster until the error manager is implemented
+    poster = Poster.find_by(id: params[:id])
+
+    if poster
+      poster.destroy
+      head :no_content
+    else
+      render json: { error: "Poster not found" }, status: :not_found
+    end
+  end
+  
+  # def destroy
+  #   # Koiree: Let Error Manager handle if the poster is not found
+  #   poster = Poster.find(params[:id])
+
+  #   # Koiree: Destroy the record and return 204 No Content
+  #   poster.destroy
+  #   head :no_content
+  # end
 
   private
   
