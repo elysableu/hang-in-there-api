96e1c6a2afbeb73b9c93712075ec3898c4aceebd
elysableu <elysabward@gmail.com>
2024-12-19 15:59:44 -0700

    Add meta to posters json object

--- app/serializers/poster_serializer.rb
+++ app/serializers/poster_serializer.rb
@@ -1,22 +1,23 @@
 class PosterSerializer
-  def self.format_posters(posters)
+  def self.format_posters(posters, count)
     {
       data: posters.map do |poster|
         formatter(poster) # Calls the formatter method for each poster
-      end
+      end,
+      meta: count
     }
   end
 
  # Format the poster to match the desired JSON structure
   def self.format_poster(poster)
     {
-      data: formatter(poster)   
+      data: formatter(poster)
     }
   end
  
   def self.formatter(poster)
     {
-      id: poster.id,
+      id: poster.id.to_s,
       type: "poster",
       attributes: {
         name: poster.name,
